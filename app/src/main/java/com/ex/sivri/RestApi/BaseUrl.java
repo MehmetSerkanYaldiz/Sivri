284c897112594e3905192b045012a9b6b2bfb018
Mehmet Serkan Yaldız <58881108+MehmetSerkanYaldiz@users.noreply.github.com>
2020-07-13 12:57:30 +0300

    update2

--- app/src/main/java/com/ex/sivri/RestApi/BaseUrl.java
+++ app/src/main/java/com/ex/sivri/RestApi/BaseUrl.java
@@ -1,5 +1,5 @@
 package com.ex.sivri.RestApi;
 
 public class BaseUrl {
-    public static final String bilgi_URL="https://mehmetserkanyaldiz.000webhostapp.com";
+    public static final String bilgi_URL="https://sisifosappp.000webhostapp.com";
 }
--- app/src/main/java/com/ex/sivri/RestApi/RestApi.java
+++ app/src/main/java/com/ex/sivri/RestApi/RestApi.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface RestApi {
-    @GET("/kocİndex.php")
+    @GET("/kocindex.php")
     Call<Bilgiler> kocBilgi();
 
     @GET("/bogaindex.php")
